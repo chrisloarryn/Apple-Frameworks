5ce65ca0ebbdf2ee6484145dd5f0a2d3081cc777
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-17 19:36:54 -0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Cristóbal Contreras on 17-09-23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
